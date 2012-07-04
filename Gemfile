b492912e15ad1007e74b472c7fd24ef04109ee2f
Vickash Mahabir <vickashmahabir@gmail.com>
2012-07-04 00:50:58 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'rb-fsevent', '0.9.1'
+gem 'daemons',    '1.1.8'
---
+++ folder_watcher.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'yaml'
+Bundler.require
+
+# Load the settings from YAML into a hash.
+SETTINGS = YAML.load_file('settings.yml')
+
+# Match an FS Event path to its root folder.
+def match_root_folder(path)
+  SETTINGS.each_key do |key|
+    return SETTINGS[key] if path.index(SETTINGS[key]['path'])
+  end
+  return nil
+end
+
+# Define watcher options and paths to watch.
+options = {:latency => '5', :no_defer => false}
+paths = []
+SETTINGS.each_key { |key| paths = paths + [SETTINGS[key]['path']] }
+
+# Define the watcher.
+fsevent = FSEvent.new
+fsevent.watch paths, options do |dirs|
+  applied_settings = []
+  fsevent.stop
+  dirs.each do |dir|  
+    settings = match_root_folder(dir)
+    if settings && !(applied_settings.include? settings)
+      applied_settings = applied_settings + [settings]
+      system settings['command']
+    end
+  end
+  fsevent.run
+end
+
+# Daemonize the process.
+Daemons.daemonize
+
+# Run the FSEvent watcher.
+fsevent.run
